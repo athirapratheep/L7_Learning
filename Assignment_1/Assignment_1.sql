ab2061ff99f8fe816baa216fc0d3389aa1c66e64
Athira Pratheep <athirapratheep@mac.local>
2025-06-11 09:57:35 +0530

    changes in assignment 1

--- Assignment_1/Assignment_1.sql
+++ Assignment_1/Assignment_1.sql
@@ -1,29 +1,32 @@
 CREATE TABLE Students (
-student_ID INT PRIMARY KEY,
-name VARCHAR(25),
-email VARCHAR(40),
+student_ID INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE,
 dob DATE,
 advisor_ID INT);
 
 CREATE TABLE Advisors (
-advisor_ID INT PRIMARY KEY,
-name VARCHAR(25),
-email VARCHAR(40),
-specialization VARCHAR(25));
+advisor_ID INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE,
+specialization VARCHAR(50));
 
 CREATE TABLE Courses (
-course_code INT PRIMARY KEY,
-title VARCHAR(25), 
+course_code INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(50) NOT NULL, 
 description VARCHAR(100),
-instructor VARCHAR(25));
+instructor VARCHAR(50));
 
 
 ALTER TABLE Students 
-ADD FOREIGN KEY(advisor_ID) REFERENCES Advisors(advisor_ID) ON DELETE SET NULL;
+ADD CONSTRAINT fk_advisor_id FOREIGN KEY(advisor_ID) REFERENCES Advisors(advisor_ID) ON DELETE SET NULL;
 
 CREATE TABLE StudentCourses(
-student_ID INT NOT NULL,
+student_ID INT AUTO_INCREMENT NOT NULL,
 course_code INT NOT NULL,
+status VARCHAR(50),
+enrollmentdate DATE,
 PRIMARY KEY(student_ID,course_code),
-FOREIGN KEY(student_ID) REFERENCES Students(student_ID) ON DELETE CASCADE,
-FOREIGN KEY(course_code) REFERENCES Courses(course_code) ON DELETE CASCADE);
+CONSTRAINT fk_student_id FOREIGN KEY(student_ID) REFERENCES Students(student_ID) ON DELETE CASCADE,
+CONSTRAINT fk_course_code FOREIGN KEY(course_code) REFERENCES Courses(course_code) ON DELETE CASCADE);
+
